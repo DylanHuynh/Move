281a97b112b75341c6db141322c8f035e0644faf
Evan Hu <evanhu@berkeley.edu>
2023-10-28 15:01:50 -0700

    P1 LLM BACKEND

--- backend/llm/getContext.mjs
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import { getEmbeddingFromHF } from "./getEmbedding";
-import { getClient, queryVector } from "./vectorDB.mjs"
-
-const client = getClient()
-
-console.log(queryVector("test"));
