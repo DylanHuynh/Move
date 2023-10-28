7f6b4184d4705f13200e9a902c68c8816ec2035c
Evan Hu <evanhu@berkeley.edu>
2023-10-28 08:52:51 -0700

    morning

--- backend/llm/getMoveTitle.js
+++ backend/llm/getMoveTitle.js
@@ -7,18 +7,19 @@ const API_TOKEN = process.env.TOGETHER_API_TOKEN;
 const MODEL = "garage-bAInd/Platypus2-70B-instruct"
 
 async function getMoveTitle(eventDescription) {
+  const body = `{"model": "${MODEL}", "prompt": "${"Description: " + eventDescription + "\n" + prompts.generateMoveTitlePrompt}", "temperature": 0.7, "topP": 0.7, "topK": 50}`
+  console.log(body);
   const response = await fetch(
       ENDPOINT,
       {
-          headers: { Authorization: `Bearer ${API_TOKEN}`, "Content-Type": "application/json"},
+          headers: { Authorization: `Bearer ${API_TOKEN}`, "Content-Type": "application/json" },
           method: "POST",
-          body: `{"model": "${MODEL}", "prompt": "${"Description: " + eventDescription + "\n" prompts.generateMoveTitlePrompt}", "temperature": 0.7, "topP": 0.7, "topK": 50}`
+          body
       }
   );
   const result = await response.json();
   console.log(result)
   return result.output.choices[0].text;
-}
+};
 
 module.exports = getMoveTitle;
-
