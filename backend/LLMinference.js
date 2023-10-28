fe8c8f2eea53e706e9afa7c1a0577ae59c6fc6a8
Evan Hu <evanhu@berkeley.edu>
2023-10-27 21:15:38 -0700

    start

--- backend/LLMinference.js
+++ backend/LLMinference.js
@@ -6,11 +6,9 @@ const resolvers = require('./resolvers');
 async function startServer() {
   const app = express();
   const server = new ApolloServer({ typeDefs, resolvers });
-
-  await server.start();
   server.applyMiddleware({ app });
-
-  const PORT = process.env.PORT || 4001;
+server
+  const PORT = process.env.PORT || 4002;
   
   app.listen(PORT, () => {
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
