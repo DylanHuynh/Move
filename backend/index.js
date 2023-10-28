23ac894194a3fffb2dbb10b66b3cd6b9becb57e4
Caleb Kim <calebkim@Calebs-MacBook-Pro.local>
2023-10-28 08:47:38 -0700

    fixed

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const { ApolloServer } = require('apollo-server-express');
-const typeDefs = require('./typeDefs');  // Assuming this exports a string or array of strings
+const typeDefs = require('./schema.graphql');
 const resolvers = require('./resolvers');
-const { createMove } = require('./moveService');  // Make sure createMove is exported from moveService.js
 
 async function startServer() {
   const app = express();
-  app.use(bodyParser.json());
-
   const server = new ApolloServer({ typeDefs, resolvers });
+
   await server.start();
   server.applyMiddleware({ app });
 
-  app.post('/save-events', async (req, res) => {
-    try {
-      const events = req.body;
-      for (const event of events) {
-        console.log(event);
-        await createMove(event);
-      }
-      res.send('Events saved successfully');
-    } catch (error) {
-      console.error('Error saving events:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 4001;
+  
   app.listen(PORT, () => {
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   });
