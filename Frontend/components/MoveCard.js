479c3b8f5e3bba80a2f06a8a1c991e5cffcbd681
Clara Tu <claratu@berkeley.edu>
2023-10-28 23:35:49 -0700

    move card component

--- Frontend/components/MoveCard.js
+++ Frontend/components/MoveCard.js
@@ -4,49 +4,101 @@ import { Card, Button } from "react-native-paper";
 
 import { useNavigation } from '@react-navigation/native';
 
-export default function MoveCard(item, type) {
-    const navigation = useNavigation();
-    const imgSource = { uri: "https://picsum.photos/700" };
-    const moveObj = item['move'];
+export default function MoveCard({item}) {
+  const moveObj = item["move"];
+  const [modalVisible, setModalVisible] = useState(false);
 
-    const goToChat = () => {
-        console.log("changing to chat");
-        navigation.navigate('Chat', { chatId: moveObj.chatId});
-    }
-
-    // const saveAndGoToProfile = () => {
-        // useQuery(gql`
-        //     createMove {
-        //         getChat(id) {
-        //             id,
-        //             friendIds,
-        //             messageIds,
-        //             creationTime,
-        //             owner,
-        //             type,
-        //             messages,
-        //             moves,
-        //         }
-        //     }
-        // `);
-
-
-    //     navigation.navigate('Profile');
-    // }
-
-    return (
-      <View className="bg-white w-full py-4">
-        <Card>
-          <Card.Cover source={imgSource} />
-          <Card.Title title={moveObj.title} subtitle={moveObj.time} />
-          <Card.Content>
-            <Text variant="bodyMedium">{moveObj.description}</Text>
-          </Card.Content>
-          <Card.Actions>
-            {type == "explore" ? <Button>move!</Button> : <Button onPress={goToChat}>Message!</Button> }
-
-          </Card.Actions>
-        </Card>
-      </View>
-    );
+  const acceptMove = () => {
+    setModalVisible(!modalVisible);
   };
+  const declineMove = () => {
+    setModalVisible(!modalVisible);
+  };
+
+  return (
+    <View className="bg-background-color w-full py-4">
+      <Card className="bg-white ">
+        <Card.Cover className="rounded-b-none h-[14vh]" source={imgSource} />
+        <View className="px-2">
+          <Card.Title
+          className="font-bold"
+          title={moveObj.title}
+          subtitle={moveObj.location + " @ " + moveObj.time}
+          titleVariant="titleMedium"
+          titleStyle={{ fontWeight: "bold" }}
+          subtitleVariant="bodyLarge"
+        />
+        <Card.Content>
+          <Text variant="bodyMedium" numberOfLines={2}>{moveObj.description}</Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button
+            className="bg-primary-color border-transparent my-2"
+            textColor="black"
+            onPress={() => setModalVisible(true)}
+          >
+            move!
+          </Button>
+        </Card.Actions>
+        </View>
+        
+      </Card>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View className="h-full bg-black/50">
+          <View className="mt-auto h-[90vh] bg-background-color justify-between rounded-t-full">
+            <View className="rounded-t">
+              <Image className="h-[23vh] rounded-t-xl" source={imgSource} />
+              <View className="flex flex-row-reverse">
+                <IconButton
+                  icon="close-circle-outline"
+                  onPress={() => setModalVisible(!modalVisible)}
+                />
+              </View>
+              <ScrollView className="px-10">
+                <Text variant="headlineMedium" className="font-bold py-2">{moveObj.title}</Text>
+                <Text variant="titleMedium">
+                  Date and Time: {moveObj.time}
+                </Text>
+                <Text variant="titleMedium" className="py-2" >Address: {moveObj.location}</Text>
+                <Text variant="bodyMedium" >{moveObj.description}</Text>
+                {moveObj.link != null ?
+                <Text
+                  variant="bodyMedium"
+                  className="text-cyan-700 py-2"
+                  onPress={() => Linking.openURL(moveObj.link)}
+                >
+                  Link to Event!
+                </Text>
+                : <></>}
+              </ScrollView>
+            </View>
+
+            <View className="flex flex-row justify-center p-2">
+              <Button
+                textColor="white"
+                className="bg-primary-color w-1/3 m-10"
+                onPress={acceptMove}
+              >
+                Accept
+              </Button>
+              <Button
+                textColor="black"
+                className="bg-secondary-color w-1/3 m-10"
+                onPress={declineMove}
+              >
+                Cancel
+              </Button>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
--- Frontend/pages/Explore.js
+++ Frontend/pages/Explore.js
@@ -8,6 +8,8 @@ import {
   ScrollView,
 } from "react-native";
 import { Card, Button, IconButton, Text } from "react-native-paper";
+import MoveCard from "../components/MoveCard";
+
 
 export default function Explore() {
   const moves = [
@@ -48,105 +50,6 @@ export default function Explore() {
       title: "clink with the gals!",
     },
   ];
-
-  const MoveCard = (item) => {
-    const moveObj = item["move"];
-    const [modalVisible, setModalVisible] = useState(false);
-
-    const acceptMove = () => {
-      setModalVisible(!modalVisible);
-    };
-    const declineMove = () => {
-      setModalVisible(!modalVisible);
-    };
-
-    return (
-      <View className="bg-background-color w-full py-4">
-        <Card className="bg-white ">
-          <Card.Cover className="rounded-b-none h-[14vh]" source={imgSource} />
-          <View className="px-2">
-            <Card.Title
-            className="font-bold"
-            title={moveObj.title}
-            subtitle={moveObj.location + " @ " + moveObj.time}
-            titleVariant="titleMedium"
-            titleStyle={{ fontWeight: "bold" }}
-            subtitleVariant="bodyLarge"
-          />
-          <Card.Content>
-            <Text variant="bodyMedium" numberOfLines={2}>{moveObj.description}</Text>
-          </Card.Content>
-          <Card.Actions>
-            <Button
-              className="bg-primary-color border-transparent my-2"
-              textColor="black"
-              onPress={() => setModalVisible(true)}
-            >
-              move!
-            </Button>
-          </Card.Actions>
-          </View>
-          
-        </Card>
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            setModalVisible(!modalVisible);
-          }}
-        >
-          <View className="h-full bg-black/50">
-            <View className="mt-auto h-[90vh] bg-background-color justify-between rounded-t-full">
-              <View className="rounded-t">
-                <Image className="h-[23vh] rounded-t-xl" source={imgSource} />
-                <View className="flex flex-row-reverse">
-                  <IconButton
-                    icon="close-circle-outline"
-                    onPress={() => setModalVisible(!modalVisible)}
-                  />
-                </View>
-                <ScrollView className="px-10">
-                  <Text variant="headlineMedium" className="font-bold py-2">{moveObj.title}</Text>
-                  <Text variant="titleMedium">
-                    Date and Time: {moveObj.time}
-                  </Text>
-                  <Text variant="titleMedium" className="py-2" >Address: {moveObj.location}</Text>
-                  <Text variant="bodyMedium" >{moveObj.description}</Text>
-                  {moveObj.link != null ?
-                  <Text
-                    variant="bodyMedium"
-                    className="text-cyan-700 py-2"
-                    onPress={() => Linking.openURL(moveObj.link)}
-                  >
-                    Link to Event!
-                  </Text>
-                  : <></>}
-                </ScrollView>
-              </View>
-
-              <View className="flex flex-row justify-center p-2">
-                <Button
-                  textColor="white"
-                  className="bg-primary-color w-1/3 m-10"
-                  onPress={acceptMove}
-                >
-                  Accept
-                </Button>
-                <Button
-                  textColor="black"
-                  className="bg-secondary-color w-1/3 m-10"
-                  onPress={declineMove}
-                >
-                  Cancel
-                </Button>
-              </View>
-            </View>
-          </View>
-        </Modal>
-      </View>
-    );
-  };
 
   const renderMoveCard = (item) => {
     return <MoveCard move={item["item"]} />;
